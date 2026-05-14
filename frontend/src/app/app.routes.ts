2d3d1e5f5bc884657adc723a19f644ffbff16fd6
Bastián Poloni <128436347+bastianpoloni@users.noreply.github.com>
2026-05-13 20:41:55 -0400

    Se fabrican las rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CategoriesComponent } from './features/categorias/components/categorias/categorias.component';
+import { DashboardComponent } from './features/dashboard/components/dashboard/dashboard.component';
+import { DetailComponent } from './features/detalle/components/detalle/detalle.component';
+import { HistoryComponent } from './features/historial/components/historial/historial.component';
+import { SettingsComponent } from './features/ajustes/components/ajustes/ajustes.component';
+
+export const routes: Routes = [
+	{
+		path: '',
+		component: DashboardComponent,
+		title: 'ChanchitoApp | Resumen'
+	},
+	{
+		path: 'categorias',
+		component: CategoriesComponent,
+		title: 'ChanchitoApp | Categorías'
+	},
+	{
+		path: 'detalle',
+		component: DetailComponent,
+		title: 'ChanchitoApp | Detalle'
+	},
+	{
+		path: 'historial',
+		component: HistoryComponent,
+		title: 'ChanchitoApp | Histórico'
+	},
+	{
+		path: 'configuracion',
+		component: SettingsComponent,
+		title: 'ChanchitoApp | Configuración'
+	},
+	{
+		path: '**',
+		redirectTo: ''
+	}
+];
