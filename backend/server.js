cd8b3ee1d0e3e8d44d048bc1d2994f9769678620
Bastián Poloni <128436347+bastianpoloni@users.noreply.github.com>
2026-05-15 15:02:55 -0400

    correcciones server

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const db = admin.firestore();
 app.use(express.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     if (req.method === 'OPTIONS') {
         return res.sendStatus(204);
